2374d001bae4400b30e974e28b19a25944ccb7f1
Tony Kornmeier <akornmeier@users.noreply.github.com>
2026-03-19 12:16:10 -0400

    chore: update the claude orchestration command

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -6,4 +6,4 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 # Claude Code Orchestration
-alias cldyo='export CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS=1 && claude -p --dangerously-skip-permissions --model opus'
+alias cldyo='export CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS=1 && claude --dangerously-skip-permissions --model opus --teammate-mode tmux'
